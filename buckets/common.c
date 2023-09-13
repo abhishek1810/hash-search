6dd00c3e55d45a8de04b4a7f2d8c8a12de3a68b0
biz-cloud-abhishek <120293411+abhishekBCE@users.noreply.github.com>
2023-09-13 11:47:29 -0500

    buckets code added

---
+++ buckets/common.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+const int HASH_SIZE = 9;
+const int PREFIX_SIZE = 1;
+const int SEARCH_COUNT = 1000;
+const int hashesPerBucket = 256;
+const size_t hashesPerBucketRead = 256 * 256;
+
+struct hashObject
+{
+    char byteArray[HASH_SIZE - PREFIX_SIZE];
+    long int value;
+};
+
+void printArray(unsigned char byteArray[HASH_SIZE], int arraySize)
+{
+    printf("printArray(): ");
+    for (size_t i = 0; i < arraySize; i++)
+    {
+        printf("%02x ", byteArray[i]); // Print each byte in hexadecimal format
+    }
+    printf("\n");
+}
